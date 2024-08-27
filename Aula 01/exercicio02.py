43a07a97574bee3ddef6394d1973e0a3544643f4
Renan Marx <73027928+renanmarxx@users.noreply.github.com>
2024-08-27 01:59:50 -0300

    Delete Aula 01 directory

--- Aula 01/exercicio02.py
+++
@@ -1,8 +0,0 @@
-# Crie um programa onde o usuário informe dois valores e em seguida fornecesse a soma destes valores.
-
-valor_1 = int(input("Informe o primeiro valor: "))
-valor_2 = int(input("Informe o segundo valor: "))
-soma_valores = valor_1 + valor_2
-
-print(f"O primeiro valor informado foi {valor_1} e o segundo valor informado foi {valor_2}")
-print(f"Realizando a soma: {valor_1} + {valor_2} = {soma_valores}")
