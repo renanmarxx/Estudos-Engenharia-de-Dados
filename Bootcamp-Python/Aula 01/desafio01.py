34c385c6ff4ec289e5c2e51bdf89ce68171b11f3
Renan Marx <renanmarx@icloud.com>
2026-04-03 16:58:42 -0300

    feat: atualizando o desafio01 e modificacoes no ambiente virtual

--- Bootcamp-Python/Aula 01/desafio01.py
+++ Bootcamp-Python/Aula 01/desafio01.py
@@ -4,17 +4,51 @@ o programa então deve imprimir uma mensagem saudando o usuário pelo nome e inf
 do salário em comparação com o bônus recebido 
 """
 
-nome = str(input("Digite o seu nome: "))
-salario = float(input("Informe o valor do seu salário: "))
-bonus = float(input("Informe o valor que você recebeu de bônus: "))
-
 CONSTANTE_BONUS = 1000
 
-calculo_bonus = CONSTANTE_BONUS + salario * bonus
+def recebe_nome() -> str:
+    nome = str(input("Digite o seu nome: "))
+    if not nome:
+        raise ValueError("O nome não pode ser vazio.")
+    return nome
 
-string_mensagem = f""""
-Olá, {nome}!\n
-Segue o valor do seu bônus a ser recebido: R$ {calculo_bonus}
-"""
+def valida_salario() -> float:
+    salario = float(input("Informe o valor do seu salário: "))
+    
+    if salario < 0:
+        raise ValueError("O salário não pode ser negativo.")
+    return salario
 
-print(string_mensagem)
+def valida_bonus() -> float:
+    bonus = float(input("Informe o valor que você recebeu de bônus: "))
+
+    if bonus < 0:
+        raise ValueError("O bônus não pode ser negativo.")
+    return bonus
+
+def calcula_bonus(salario: float, bonus: float, CONSTANTE_BONUS: float) -> float:
+
+    return CONSTANTE_BONUS + (salario * bonus)
+
+def executa_regra(nome: str, salario: float, bonus: float) -> None:
+
+    calculo_bonus = calcula_bonus(salario, bonus, CONSTANTE_BONUS)  
+    
+    string_mensagem = f"""
+    Olá, {nome}!\n
+    Segue o valor do seu bônus a ser recebido: R$ {calculo_bonus}
+    """
+
+    print(string_mensagem)
+
+def main():
+    try:
+        nome = recebe_nome()
+        salario = valida_salario()
+        bonus = valida_bonus()
+        executa_regra(nome, salario, bonus)
+    except ValueError as e:
+        print(f"Erro: {e}")
+
+if __name__ == "__main__":
+    main()
