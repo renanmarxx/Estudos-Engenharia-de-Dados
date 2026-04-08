ddc34daa94db746e1f8f04e5c837ef1b1d6d7def
Renan Marx <renanmarx@icloud.com>
2026-04-07 23:07:23 -0300

    feat: Alterando o desafio02.py

--- Bootcamp-Python/Aula 02 - TypeError, Type Check, Type Conversion, try-except e if/desafio02.py
+++ Bootcamp-Python/Aula 02 - TypeError, Type Check, Type Conversion, try-except e if/desafio02.py
@@ -1,3 +1,5 @@
+CONSTANTE_BONUS = 1000
+
 # Solicita ao usuário que digite seu nome
 try:
     nome = input("Digite seu nome: ")
@@ -10,8 +12,11 @@ try:
     elif any(char.isdigit() for char in nome):
         raise ValueError("O nome não deve conter números.")
         exit()
+    elif nome.isspace():
+        raise ValueError("Voce digitou apenas um espaço")
     else:
         print("Nome válido:", nome)
+
 except ValueError as e:
     print(e)
     exit()
@@ -35,7 +40,7 @@ except ValueError:
     print("Entrada inválida para o bônus. Por favor, digite um número.")
     exit()
 
-bonus_recebido = 1000 + salario * bonus  # Exemplo simples de KPI
+bonus_recebido = CONSTANTE_BONUS + (salario * bonus)
 
 # Imprime as informações para o usuário
 print(
